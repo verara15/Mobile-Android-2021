4ed54f76824a4f762590ec917fb0a2373a19630f
Vera <vera1@student.umn.ac.id>
2021-05-01 20:40:31 +0700

    commit

---
+++ Week11_28777_Retrofit/app/src/main/java/umn/ac/id/week11_28777_retrofit/JsonPlaceHolderApi.java
@@ -0,0 +1,12 @@
+package umn.ac.id.week11_28777_retrofit;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
